2d843f80b29bb3db91324307c31bd8ef448fa08b
Gokhan Kurt <krtgkn@gmail.com>
2024-01-07 16:38:27 +0300

    update yarn for sub packages

--- examples/vite/src/App.tsx
+++ examples/vite/src/App.tsx
@@ -1,20 +1,12 @@
-import { PropsWithChildren, useState } from 'react';
+import { PropsWithChildren } from 'react';
 import './App.css';
 
 function App({ children }: PropsWithChildren) {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>JSX Middlewares with Vite</h1>
       <p className='read-the-docs' $tooltip='Hello there'>
-        Click on the Vite and React logos to learn more
+        Hover here to see the tooltip
       </p>
       {children}
     </>
